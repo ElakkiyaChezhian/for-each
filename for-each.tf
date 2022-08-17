80dd2e2d0b283e1714bafae45d49f16ff4eee423
ElakkiyaChezhian <87109565+ElakkiyaChezhian@users.noreply.github.com>
2022-08-17 11:14:23 +0530

    Update for-each.tf

--- for-each.tf
+++ for-each.tf
@@ -57,7 +57,8 @@ location     = each.value
 org_id   = google_apigee_organization.apigeex_org.id
 }
 resource "google_apigee_instance_attachment" "instance_attachment" {
-  instance_id  = google_apigee_instance.apigee_instance1-${each.value}.id
+  for_each     =toset(apigee_instance1.name)
+  instance_id  = google_apigee_instance.apigee_instance1.id
   environment  = google_apigee_environment.apigee_org_region_env1.name
 }
 resource "google_compute_region_backend_service" "producer_service_backend1" {
